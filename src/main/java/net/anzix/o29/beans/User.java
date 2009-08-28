4e8fe2d592b67447723574522866d3de28afd88b
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-08-28 18:58:10 +0000

    user extends basebean

--- src/main/java/net/anzix/o29/beans/User.java
+++ src/main/java/net/anzix/o29/beans/User.java
@@ -2,17 +2,13 @@ package net.anzix.o29.beans;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="o29user")
 @XmlRootElement(name="user")
-public class User {
-	@Id
-	long id;
-	@Column(nullable=false, updatable=false, length=64)
+public class User extends BaseBean {
 	String displayName;
 	@Column(nullable=true, length=64)
 	String email;
