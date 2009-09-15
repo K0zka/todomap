e7fbb90f12bae970a4d32361823bf9300b1e9b75
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-09-15 06:46:37 +0000

    http://code.google.com/p/todomap/issues/detail?id=7 Rating logic and JSR 311 annotations

--- trunk/src/main/java/net/anzix/o29/logic/RatingService.java
+++ trunk/src/main/java/net/anzix/o29/logic/RatingService.java
@@ -1,5 +1,15 @@
 package net.anzix.o29.logic;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+@Consumes("application/json")
+@Path("/rating/")
 public interface RatingService {
-	void addRating(long id, short rating, String comment);
+	@POST
+	@Path("/add/{id}")
+	void addRating(@PathParam("id") long id, @QueryParam("rating") short rating, @QueryParam("comment") String comment);
 }
