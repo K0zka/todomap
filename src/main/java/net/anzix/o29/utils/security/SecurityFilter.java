742c5c9973f0d2b9862ff10e9d8e26bf48850ab0
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-10-16 06:38:37 +0000

    securityFilter checks user for existence and creates it if it does not exist

--- src/main/java/net/anzix/o29/utils/security/SecurityFilter.java
+++ src/main/java/net/anzix/o29/utils/security/SecurityFilter.java
@@ -5,36 +5,46 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.anzix.o29.beans.User;
+import net.anzix.o29.logic.UserService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.Authentication;
 import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class SecurityFilter implements Filter {
 
 	private final static Logger logger = LoggerFactory
 			.getLogger(SecurityFilter.class);
 
+	private ServletContext servletContext;
+
 	@Override
 	public void destroy() {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		Authentication authentication = SecurityContextHolder.getContext()
-				.getAuthentication();
+	public void doFilter(final ServletRequest request,
+			final ServletResponse response, final FilterChain chain)
+			throws IOException, ServletException {
+
+		final Authentication authentication = SecurityContextHolder
+				.getContext().getAuthentication();
 		final String method = ((HttpServletRequest) request).getMethod();
-		if (authentication == null
-				&& ("POST".equalsIgnoreCase(method)
-						|| "PUT".equalsIgnoreCase(method) || "DELETE"
-						.equalsIgnoreCase(method))) {
+		final boolean protectedMethod = isProtectedMethod(method);
+
+		checkUser(authentication); 
+		
+		if (authentication == null && protectedMethod) {
 			((HttpServletResponse) response)
 					.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			((HttpServletResponse) response).setContentType("application/json");
@@ -46,9 +56,33 @@ public class SecurityFilter implements Filter {
 
 	}
 
+	private void checkUser(final Authentication authentication) {
+		/* check user in DB */
+		if(authentication != null) {
+			final String openIdUrl = authentication.getName();
+			UserService userService = getUserService();
+			User userByOpenIdUrl = userService.getUserByOpenIdUrl(openIdUrl);
+			if(userByOpenIdUrl == null) {
+				userByOpenIdUrl = new User();
+				userByOpenIdUrl.setOpenIdUrl(openIdUrl);
+				userService.addUser(userByOpenIdUrl);
+			}
+		}
+	}
+
+	private UserService getUserService() {
+		return (UserService) WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext).getBean("users");
+	}
+
+	private boolean isProtectedMethod(final String method) {
+		return ("POST".equalsIgnoreCase(method)
+				|| "PUT".equalsIgnoreCase(method) || "DELETE"
+				.equalsIgnoreCase(method));
+	}
+
 	@Override
 	public void init(FilterConfig config) throws ServletException {
-
+		this.servletContext = config.getServletContext();
 	}
 
 }
