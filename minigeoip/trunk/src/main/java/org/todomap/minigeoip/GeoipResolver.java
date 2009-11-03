73cdbca7abc15262fb84bb1908c68560ac324e25
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-11-03 22:18:28 +0000

    javadoc

--- minigeoip/trunk/src/main/java/org/todomap/minigeoip/GeoipResolver.java
+++ minigeoip/trunk/src/main/java/org/todomap/minigeoip/GeoipResolver.java
@@ -1,6 +1,23 @@
 package org.todomap.minigeoip;
 
-
+/**
+ * GEO Ip resolver interface.
+ * 
+ * @author kocka
+ *
+ */
 public interface GeoipResolver {
+	/**
+	 * Resolve the country code based in IP address.
+	 * @param address stringified IP-address
+	 * @return	2-letter country code
+	 */
 	String getCountryCode(final String address);
+	
+	/**
+	 * Update the datasource. Do nothing if not required.
+	 * 
+	 * @throws Exception
+	 */
+	void update() throws Exception;
 }
