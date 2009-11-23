a3f3e902a4eebe1cf1da19ad9ef6e913afda9545
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-11-23 11:09:36 +0000

    http://code.google.com/p/todomap/issues/detail?id=11
    this feature urgently needs a UI cleanup

--- trunk/src/main/java/org/todomap/o29/logic/CommentService.java
+++ trunk/src/main/java/org/todomap/o29/logic/CommentService.java
@@ -2,21 +2,22 @@ package org.todomap.o29.logic;
 
 import java.util.List;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.xml.bind.annotation.XmlElementWrapper;
 
 import org.todomap.o29.beans.Comment;
 
 
 @Path("/comments/")
+@Produces("application/json")
 public interface CommentService {
-	@Path("/add")
+	@Path("/add/{id}")
 	@POST
-	void addComment(@FormParam("id") long id, @FormParam("text") String text);
+	void addComment( @PathParam("id") long id, String comment);
 
 	@GET
 	@Path("/get/{id}")
