ec1bdf66ed2f46603d0a09668638f3bf84eab157
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2009-12-08 19:59:34 +0000

    http://code.google.com/p/todomap/issues/detail?id=78

--- trunk/src/main/java/org/todomap/o29/logic/Configuration.java
+++ trunk/src/main/java/org/todomap/o29/logic/Configuration.java
@@ -2,6 +2,25 @@ package org.todomap.o29.logic;
 
 public class Configuration {
 	boolean debugEnabled = false;
+	String googleWebmastersVerification = "";
+	String yahooSiteExplorerVerification = "";
+
+	public String getGoogleWebmastersVerification() {
+		return googleWebmastersVerification;
+	}
+
+	public void setGoogleWebmastersVerification(String googleWebmastersVerification) {
+		this.googleWebmastersVerification = googleWebmastersVerification;
+	}
+
+	public String getYahooSiteExplorerVerification() {
+		return yahooSiteExplorerVerification;
+	}
+
+	public void setYahooSiteExplorerVerification(
+			String yahooSiteExplorerVerification) {
+		this.yahooSiteExplorerVerification = yahooSiteExplorerVerification;
+	}
 
 	public boolean isDebugEnabled() {
 		return debugEnabled;
@@ -10,4 +29,5 @@ public class Configuration {
 	public void setDebugEnabled(boolean debugEnabled) {
 		this.debugEnabled = debugEnabled;
 	}
+	
 }
