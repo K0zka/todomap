31f5e9a6ee339105d9a0aff02346cc7c30f625fb
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-01-23 07:48:46 +0000

    added cache control header

--- src/main/java/org/todomap/o29/utils/GeoipServlet.java
+++ src/main/java/org/todomap/o29/utils/GeoipServlet.java
@@ -1,6 +1,7 @@
 package org.todomap.o29.utils;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +12,18 @@ public class GeoipServlet extends SpringServlet {
 	private static final long serialVersionUID = 4180666820704962665L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
 			throws ServletException, IOException {
 		final String countryCode = geoipResolver.getCountryName(req.getRemoteAddr());
-		resp.getWriter().write(countryCode == null ? "unknown" : countryCode);
+		final PrintWriter writer = resp.getWriter();
+		resp.setHeader("Cache-control", "max-age=3600");
+		if(req.getRequestURI().endsWith(".js")) {
+			writer.write("org.todomap.geoip='");
+			writer.write(countryCode == null ? "unknown" : countryCode);
+			writer.write("';\n");
+		} else {
+			writer.write(countryCode == null ? "unknown" : countryCode);
+		}
 	}
 
 }
