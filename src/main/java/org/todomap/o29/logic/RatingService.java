a30b0020e75df02038d577ab799e34292c53efde
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-02-08 22:29:09 +0000

    http://code.google.com/p/todomap/issues/detail?id=85

--- src/main/java/org/todomap/o29/logic/RatingService.java
+++ src/main/java/org/todomap/o29/logic/RatingService.java
@@ -5,11 +5,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import org.todomap.o29.beans.Rating;
 
 
 @Consumes("application/json")
+@Produces("application/json")
 @Path("/rating/")
 public interface RatingService {
 	@POST
