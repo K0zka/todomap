a79c3a281fbb8afcd3cc35f015807c3717246fa8
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-02-08 20:38:43 +0000

    jquery tooltip replaced by qtip

--- trunk/src/main/webapp/scripts/o29.js
+++ trunk/src/main/webapp/scripts/o29.js
@@ -9,7 +9,8 @@ var bookmarks = [];
 /**
  * Tries to find out if a variable is defined or not.
  * 
- * @param variable name of the variable
+ * @param variable
+ *            name of the variable
  * @return true or false
  */
 function isDefined(variable) {
@@ -18,7 +19,9 @@ function isDefined(variable) {
 
 /**
  * Debug the message on the firebug console, if available.
- * @param message the message to log
+ * 
+ * @param message
+ *            the message to log
  * @return nothing
  */
 function debug(message) {
@@ -53,6 +56,7 @@ function zoomLevel() {
 
 /**
  * Check the login status and hide/show UI items based on this info.
+ * 
  * @return nothing
  */
 function checkLoginStatus() {
@@ -65,7 +69,7 @@ function checkLoginStatus() {
 		success : function(msg) {
 			var isLoggedIn = eval("(" + msg + ")");
 			try {
-				if(isLoggedIn != isAuthenticated) {
+				if (isLoggedIn != isAuthenticated) {
 					if (isLoggedIn) {
 						$("#loginWindow").dialog('close');
 						$(".authOnly").show(1000);
@@ -89,15 +93,16 @@ function checkLoginStatus() {
 
 /**
  * Call the server side to close session.
+ * 
  * @return
  */
 function logOut() {
-	$.ajax({
-			type	:	'GET',
-			url		: 'logout.jsp',
-			success : checkLoginStatus,
-			error	: checkLoginStatus
-		});
+	$.ajax( {
+		type : 'GET',
+		url : 'logout.jsp',
+		success : checkLoginStatus,
+		error : checkLoginStatus
+	});
 }
 
 function checkLoginStatus_periodically() {
@@ -108,125 +113,158 @@ function checkLoginStatus_periodically() {
 checkLoginStatus_periodically();
 
 function refreshBookmarks() {
-	$.get('services/bookmarks', 
-		function(data) {
-			bookmarks = eval('('+data+')');
-			$('#bookmarks').empty();
-			$.each(bookmarks['bookmark'], function(i, val) {
-				$('#bookmarks').append("<a class='bookmark' href=\""+val['id']+'-'+val['text']+".html\" target=\"_blank\">"+val['text']+"</a> <img src='img/external-link.png' onclick='' style='cursor:pointer;'/> <br/>");
-			});
-		}
-	);
+	$
+			.get(
+					'services/bookmarks',
+					function(data) {
+						bookmarks = eval('(' + data + ')');
+						$('#bookmarks').empty();
+						$
+								.each(
+										bookmarks['bookmark'],
+										function(i, val) {
+											$('#bookmarks')
+													.append(
+															"<a class='bookmark' href=\""
+																	+ val['id']
+																	+ '-'
+																	+ val['text']
+																	+ ".html\" target=\"_blank\">"
+																	+ val['text']
+																	+ "</a> <img src='img/external-link.png' onclick='' style='cursor:pointer;'/> <br/>");
+										});
+					});
 }
 
 function bookmarkItem(itemId) {
-	debug('Bookmarking '+itemId);
-	$.ajax({
-		type:	'POST',
-		url:	'services/bookmarks/bookmark/'+itemId,
-		data:	itemId,
+	debug('Bookmarking ' + itemId);
+	$.ajax( {
+		type : 'POST',
+		url : 'services/bookmarks/bookmark/' + itemId,
+		data : itemId,
 		success : refreshBookmarks,
-		error	: function(XMLHttpRequest, textStatus, errorThrown) {
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			handleErrors(XMLHttpRequest);
 		}
 	});
 }
 
 function unbookmarkItem(itemId) {
-	$.ajax({
-		type:	'POST',
-		url:	'services/bookmarks/unbookmark/'+itemId,
-		data:	itemId,
+	$.ajax( {
+		type : 'POST',
+		url : 'services/bookmarks/unbookmark/' + itemId,
+		data : itemId,
 		success : refreshBookmarks,
-		error	: function(XMLHttpRequest, textStatus, errorThrown) {
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			handleErrors(XMLHttpRequest);
 		}
 	});
 }
 
 function isBookmarked(itemId) {
-//	for(var i =0; i < bookmarks['bookmark'].) {
-//		
-//	}
+	// for(var i =0; i < bookmarks['bookmark'].) {
+	//		
+	// }
 }
 
 function isBookmarked(itemId) {
-	//TODO
+	// TODO
 	return false;
 }
 
 function unBookmarkItem(itemId) {
-	$.ajax({
-		type:	'DELETE',
-		url:	'services/bookmarks/'+itemId
+	$.ajax( {
+		type : 'DELETE',
+		url : 'services/bookmarks/' + itemId
 	});
 }
 
 function increaseCounter(name) {
-	$('#'+name).text(eval($('#'+name).text()+ '+ 1'));
+	$('#' + name).text(eval($('#' + name).text() + '+ 1'));
 }
 
 function setCounter(name, value) {
-	$('#'+name).text(value);
+	$('#' + name).text(value);
 }
 
 function deleteResource(id, callback) {
-	$.ajax({
-		type: 	'POST',
-		url: 	'services/base/remove/'+id,
-		success:callback
+	$.ajax( {
+		type : 'POST',
+		url : 'services/base/remove/' + id,
+		success : callback
 	});
 }
 
 function deleteAttachment(id, callback) {
-	$.ajax({
-		type: 	'POST',
-		url: 	'services/attachments/delete/'+id,
-		success:callback
+	$.ajax( {
+		type : 'POST',
+		url : 'services/attachments/delete/' + id,
+		success : callback
 	});
 }
 
 function updateLatLong(value, latResult, lngResult) {
-	var addr = $('#'+value).val();
+	var addr = $('#' + value).val();
 	debug(addr);
 	geocoder = new google.maps.Geocoder();
-	geocoder.geocode( { 'address': addr}, function(results, status) {
+	geocoder.geocode( {
+		'address' : addr
+	}, function(results, status) {
 		debug('status:' + status);
 		try {
-			$('#'+latResult).val(results[0]['geometry']['location'].lat());
-			$('#'+lngResult).val(results[0]['geometry']['location'].lng());
+			$('#' + latResult).val(results[0]['geometry']['location'].lat());
+			$('#' + lngResult).val(results[0]['geometry']['location'].lng());
 		} catch (err) {
-			debug('Error: '+err);
+			debug('Error: ' + err);
 		}
-      });
+	});
 }
 
 function updateAddr(addr, latInput, lngInput) {
 	geocoder = new google.maps.Geocoder();
-	var lat = $('#'+latInput).val();
-	var lng = $('#'+lngInput).val();
-	geocoder.geocode( {'latLng' : new google.maps.LatLng(lat, lng)}, function(results, status) {
+	var lat = $('#' + latInput).val();
+	var lng = $('#' + lngInput).val();
+	geocoder.geocode( {
+		'latLng' : new google.maps.LatLng(lat, lng)
+	}, function(results, status) {
 		debug('status: ' + status);
 		debug(results);
 		try {
-			if(results[0]) {
-					$('#'+addr).val(results[0].formatted_address);
+			if (results[0]) {
+				$('#' + addr).val(results[0].formatted_address);
 			}
 		} catch (err) {
-			debug('Error: '+err);
+			debug('Error: ' + err);
 		}
-      });
+	});
 }
 
 function togle(who, callback) {
-	var tog = $('#'+who).attr('class');
+	var tog = $('#' + who).attr('class');
 	var oldVal = tog.indexOf('inactive') == -1;
-	if(oldVal) {
-		$('#'+who).attr('class','starTogle_inactive');
+	if (oldVal) {
+		$('#' + who).attr('class', 'starTogle_inactive');
 	} else {
-		$('#'+who).attr('class','starTogle_active');
+		$('#' + who).attr('class', 'starTogle_active');
 	}
-	if(callback) {
+	if (callback) {
 		callback(who, !oldVal);
 	}
+}
+
+function applyTooltip(elemSelector) {
+	$(elemSelector).each(function() {
+		var exists = false;
+		var selector = '#' + $(this).attr('id') + '-tooltip';
+		$(selector).each(function() {
+			exists = true
+		});
+
+		if (exists) {
+			$(this).qtip( {
+				content : $(selector).html(),
+				style : {name : 'light'}
+			});
+		}
+	});
 }
