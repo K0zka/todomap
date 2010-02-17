b2c2b69fb1db562976fbb438f39495138dfdd11c
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-02-17 20:35:20 +0000

    under development

---
+++ trunk/src/main/java/org/todomap/o29/utils/aop/JmsIntegrationAspect.java
@@ -0,0 +1,12 @@
+package org.todomap.o29.utils.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class JmsIntegrationAspect {
+	@Pointcut("")
+	public void sendMessage() {
+		
+	}
+}
