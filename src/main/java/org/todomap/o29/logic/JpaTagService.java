eaf215cd86780f2a3d7de7d681e4c1dcd8950781
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-02-28 22:24:17 +0000

    http://code.google.com/p/todomap/issues/detail?id=139

--- src/main/java/org/todomap/o29/logic/JpaTagService.java
+++ src/main/java/org/todomap/o29/logic/JpaTagService.java
@@ -1,5 +1,7 @@
 package org.todomap.o29.logic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,9 +9,20 @@ import javax.persistence.PersistenceException;
 
 import org.springframework.orm.jpa.JpaCallback;
 import org.springframework.orm.jpa.support.JpaDaoSupport;
+import org.todomap.o29.beans.BaseBean;
 import org.todomap.o29.beans.Tag;
 
 public class JpaTagService extends JpaDaoSupport implements TagService {
+
+	BaseService baseService;
+
+	public BaseService getBaseService() {
+		return baseService;
+	}
+
+	public void setBaseService(BaseService baseService) {
+		this.baseService = baseService;
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -28,8 +41,69 @@ public class JpaTagService extends JpaDaoSupport implements TagService {
 	}
 
 	@Override
-	public void addTag(final long id, final String tag) {
-		
+	public void addTag(final long id, final String language, final String tag) {
+		final BaseBean baseBean = baseService.getById(id);
+		final Tag storedTag = getJpaTemplate().execute(new JpaCallback<Tag>() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public Tag doInJpa(final EntityManager entityManager)
+					throws PersistenceException {
+				final List<Tag> resultList = entityManager
+						.createQuery(
+								"select object(t) from "
+										+ Tag.class.getName()
+										+ " t where t.tag = :tag and t.langcode = :lang")
+						.setParameter("tag", tag)
+						.setParameter("lang", language).getResultList();
+				if (resultList.isEmpty()) {
+					Tag newTag = new Tag();
+					newTag.setLangcode(language);
+					newTag.setTag(tag);
+					entityManager.persist(newTag);
+					return newTag;
+				} else {
+					return resultList.get(0);
+				}
+			}
+		});
+		baseBean.getTags().add(storedTag);
+		getJpaTemplate().persist(baseBean);
 	}
-	
+
+	@Override
+	public List<TagCloudElem> getTagCloud(final String language) {
+		ArrayList<TagCloudElem> ret = new ArrayList<TagCloudElem>();
+		for (final Tag tag : listTags(language)) {
+			ret.add(getJpaTemplate().execute(new JpaCallback<TagCloudElem>() {
+
+				@Override
+				public TagCloudElem doInJpa(final EntityManager entityManager)
+						throws PersistenceException {
+					final Long result = (Long) entityManager.createQuery(
+							"select count(*) from " + BaseBean.class.getName()
+									+ " b where :tagid in tags").setParameter(
+							"tagid", tag.getId()).getSingleResult();
+					final TagCloudElem tagCloudElem = new TagCloudElem();
+					tagCloudElem.setTag(tag);
+					tagCloudElem.setWight(result);
+					return tagCloudElem;
+				}
+			}));
+		}
+		return ret;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Tag> listTags(final String language, final String prefix) {
+		final HashMap<String, String> params = new HashMap<String, String>();
+		params.put("lang", language);
+		params.put("tag", prefix.concat("%"));
+		return getJpaTemplate().findByNamedParams(
+				"select object(t) from " + Tag.class.getName()
+						+ " t where t.langcode = :lang and t.tag like :tag",
+				params);
+	}
+
 }
--- src/main/java/org/todomap/o29/logic/TagService.java
+++ src/main/java/org/todomap/o29/logic/TagService.java
@@ -6,16 +6,47 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import org.todomap.o29.beans.Tag;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+
 @Path("/tags/")
+@Produces("application/json")
 public interface TagService {
+	
+	@XmlElement(value="tagc")
+	public class TagCloudElem {
+		Tag tag;
+		public Tag getTag() {
+			return tag;
+		}
+		public void setTag(Tag tag) {
+			this.tag = tag;
+		}
+		public long getWight() {
+			return wight;
+		}
+		public void setWight(long wight) {
+			this.wight = wight;
+		}
+		long wight;
+	}
+	
+	@GET
+	@Path("/cloud/{lang}")
+	List<TagCloudElem> getTagCloud(@PathParam("lang") String language);
+
 	@GET
 	@Path("/list/{lang}")
 	List<Tag> listTags(@PathParam("lang") String language);
-	
+
+	@GET
+	@Path("/list/{lang}/{pref}")
+	List<Tag> listTags(@PathParam("lang") String language, @PathParam("pref") String prefix);
+
 	@POST
-	@Path("/add/{id}")
-	public void addTag(@PathParam("id") long id, String tag);
+	@Path("/add/{lang}/{id}")
+	public void addTag(@PathParam("id") long id, @PathParam("lang") String language, String tag);
 }
