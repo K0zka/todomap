5bb4a306689eb838159be1081f82af72d96df701
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-03-04 22:14:51 +0000

    html util test

--- trunk/src/test/java/org/todomap/o29/utils/HtmlUtilTest.java
+++ trunk/src/test/java/org/todomap/o29/utils/HtmlUtilTest.java
@@ -5,24 +5,42 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class HtmlUtilTest {
-	
+
+	@Test
+	public void testTextify() {
+		Assert.assertEquals(HtmlUtil.textify("<b>bla</b>bla"), "blabla");
+		Assert.assertEquals(HtmlUtil.textify("bla<br>bla"), "blabla");
+	}
+
 	@Test
 	public void testCleanup() {
-		Assert.assertEquals("hello  world!", HtmlUtil.cleanup("hello <script language='javascript'>\nalert('wrong!')</script> world!"));
-		Assert.assertEquals("hello  world!", HtmlUtil.cleanup("hello <embed/> world!"));
-		Assert.assertEquals("hello  world!", HtmlUtil.cleanup("hello <!--you cruel--> world!"));
+		Assert
+				.assertEquals(
+						"hello  world!",
+						HtmlUtil
+								.cleanup("hello <script language='javascript'>\nalert('wrong!')</script> world!"));
+		Assert.assertEquals("hello  world!", HtmlUtil
+				.cleanup("hello <embed/> world!"));
+		Assert.assertEquals("hello  world!", HtmlUtil
+				.cleanup("hello <!--you cruel--> world!"));
 	}
-	
+
 	@Test
 	public void testGetFirstParagraph() {
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("<p>lorem</p><p>ipsum</p>"));
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("lorem<br/>ipsum"));
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("lorem<br>ipsum"));
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("<div>lorem<br>ipsum</div>"));
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("<span>lorem<br>ipsum</span>"));
-		Assert.assertEquals("lorem",  HtmlUtil.getFirstParagraph("<span>lorem&nbsp;ipsum</span>"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("<p>lorem</p><p>ipsum</p>"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("lorem<br/>ipsum"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("lorem<br>ipsum"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("<div>lorem<br>ipsum</div>"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("<span>lorem<br>ipsum</span>"));
+		Assert.assertEquals("lorem", HtmlUtil
+				.getFirstParagraph("<span>lorem&nbsp;ipsum</span>"));
 	}
-	
+
 	@Test
 	public void testIsAccepted() {
 		Assert.assertTrue(HtmlUtil.isAcceptedTag("div"));
