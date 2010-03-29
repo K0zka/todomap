8ba71bdd0988bcab79148b1da36733238e6a80d4
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-03-29 05:48:50 +0000

    securityfilter also export the http request and response - hack to make anon service work

--- trunk/src/main/java/org/todomap/o29/utils/security/SecurityFilter.java
+++ trunk/src/main/java/org/todomap/o29/utils/security/SecurityFilter.java
@@ -19,6 +19,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityFilter implements Filter {
 
+	public final static ThreadLocal<HttpServletRequest> requestThreadLocal = new ThreadLocal<HttpServletRequest>();
+	public final static ThreadLocal<HttpServletResponse> responseThreadLocal = new ThreadLocal<HttpServletResponse>(); 
+	
 	private final static Logger logger = LoggerFactory
 			.getLogger(SecurityFilter.class);
 
@@ -34,24 +37,24 @@ public class SecurityFilter implements Filter {
 		final Authentication authentication = SecurityContextHolder
 				.getContext().getAuthentication();
 		final String method = ((HttpServletRequest) request).getMethod();
-		final boolean protectedMethod = isProtectedMethod(method);
-
-		if (authentication instanceof AnonymousAuthenticationToken && protectedMethod) {
+		if (authentication instanceof AnonymousAuthenticationToken && isProtectedMethod(method, ((HttpServletRequest)request).getRequestURI())) {
 			((HttpServletResponse) response)
 					.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 			((HttpServletResponse) response).setContentType("application/json");
 			response.getWriter().write("{errors:['login-required']}");
 		} else {
+			requestThreadLocal.set((HttpServletRequest) request);
+			responseThreadLocal.set((HttpServletResponse) response);
 			logger.debug(authentication instanceof AnonymousAuthenticationToken ? "[anon]" : authentication.getName());
 			chain.doFilter(request, response);
 		}
 
 	}
 
-	private boolean isProtectedMethod(final String method) {
-		return ("POST".equalsIgnoreCase(method)
+	private boolean isProtectedMethod(final String method, String uri) {
+		return (("POST".equalsIgnoreCase(method)
 				|| "PUT".equalsIgnoreCase(method) || "DELETE"
-				.equalsIgnoreCase(method));
+				.equalsIgnoreCase(method)) && uri.startsWith("/anon/"));
 	}
 
 	@Override
