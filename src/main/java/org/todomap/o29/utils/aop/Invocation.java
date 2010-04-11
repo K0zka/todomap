c943766fad767cb4b970ea260e31883a0105e7c8
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-04-11 11:30:07 +0000

    integration improvements

---
+++ src/main/java/org/todomap/o29/utils/aop/Invocation.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.todomap.o29.utils.aop;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "invocation")
+class Invocation {
+	public Invocation() {
+		super();
+	}
+
+	public Invocation(String interfaceName, String methodName,
+			Object[] arguments, Object result) {
+		super();
+		this.interfaceName = interfaceName;
+		this.methodName = methodName;
+		this.arguments = arguments;
+		this.result = result;
+	}
+
+	public Object getResult() {
+		return result;
+	}
+
+	public void setResult(Object result) {
+		this.result = result;
+	}
+
+	String interfaceName;
+
+	public String getInterfaceName() {
+		return interfaceName;
+	}
+
+	public void setInterfaceName(String interfaceName) {
+		this.interfaceName = interfaceName;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	@XmlElementWrapper(name = "arguments")
+	public Object[] getArguments() {
+		return arguments;
+	}
+
+	public Date getCallTime() {
+		return callTime;
+	}
+
+	public void setCallTime(Date callTime) {
+		this.callTime = callTime;
+	}
+
+	public void setArguments(Object[] arguments) {
+		this.arguments = arguments;
+	}
+
+	String methodName;
+	Object[] arguments;
+	Object result;
+	Date callTime;
+}
