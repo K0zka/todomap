e84d1ba3c09896e3df91ca274ce6475518d11dbe
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-04-11 11:30:07 +0000

    integration improvements

--- trunk/src/main/java/org/todomap/o29/utils/aop/JmsIntegrationAspect.java
+++ trunk/src/main/java/org/todomap/o29/utils/aop/JmsIntegrationAspect.java
@@ -1,16 +1,20 @@
 package org.todomap.o29.utils.aop;
 
-import java.io.ByteArrayOutputStream;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.StreamMessage;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.MarshalException;
 import javax.xml.bind.Marshaller;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.todomap.o29.beans.BaseBean;
@@ -21,6 +25,9 @@ import org.todomap.o29.beans.BaseBean;
  * @author kocka
  */
 public final class JmsIntegrationAspect implements MethodInterceptor {
+
+	private final static Logger logger = LoggerFactory
+			.getLogger(JmsIntegrationAspect.class);
 
 	private final JmsTemplate jmsTemplate;
 	private final String[] triggerMethods = new String[] { "add", "delete",
@@ -39,15 +46,19 @@ public final class JmsIntegrationAspect implements MethodInterceptor {
 
 			final Object[] arguments = invocation.getArguments();
 			final JAXBContext jaxbContext = JAXBContext
-					.newInstance(BaseBean.class.getPackage().getName());
+					.newInstance(Invocation.class.getPackage().getName() + ":"
+							+ BaseBean.class.getPackage().getName());
 			final Marshaller marshaller = jaxbContext.createMarshaller();
-			final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			for (final Object arg : arguments) {
-				if (arg.getClass().getPackage().getName().startsWith(
-						"org.todomap")) {
-					marshaller.marshal(arg, outputStream);
-				}
+			final StringWriter writer = new StringWriter();
+			final Method method = invocation.getMethod();
+			final Invocation inv = new Invocation(method.getDeclaringClass()
+					.getName(), method.getName(), arguments, result);
+			try {
+				marshaller.marshal(inv, writer);
+			} catch (final MarshalException marshalException) {
+				logger.error("Message could not be serialized", marshalException);
 			}
+			final String message = writer.toString();
 			jmsTemplate.send(new MessageCreator() {
 
 				@Override
@@ -55,10 +66,12 @@ public final class JmsIntegrationAspect implements MethodInterceptor {
 						throws JMSException {
 					final StreamMessage streamMessage = session
 							.createStreamMessage();
-					streamMessage.writeBytes(outputStream.toByteArray());
+					streamMessage.writeBytes(message.getBytes());
+					logger.debug("message created");
 					return streamMessage;
 				}
 			});
+			logger.debug("created message: " + message);
 		}
 		return result;
 	}
