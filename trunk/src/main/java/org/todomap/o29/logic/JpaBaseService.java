f6e4ebf23622d47b187e63f1ffb40c6fb0e1276d
laszlo.hornyak <laszlo.hornyak@e834a378-9303-11de-97c9-d5744c8abab9>
2010-07-15 06:08:36 +0000

    http://code.google.com/p/todomap/issues/detail?id=169

--- trunk/src/main/java/org/todomap/o29/logic/JpaBaseService.java
+++ trunk/src/main/java/org/todomap/o29/logic/JpaBaseService.java
@@ -1,12 +1,19 @@
 package org.todomap.o29.logic;
 
-
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.util.Version;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.orm.jpa.JpaCallback;
 import org.springframework.orm.jpa.support.JpaDaoSupport;
 import org.todomap.o29.beans.BaseBean;
@@ -14,8 +21,29 @@ import org.todomap.o29.beans.User;
 
 public class JpaBaseService extends JpaDaoSupport implements BaseService {
 
+	private final Logger logger = LoggerFactory.getLogger(JpaBaseService.class);
+
+	public void init() throws InterruptedException {
+		getJpaTemplate().execute(new JpaCallback<Object>() {
+
+			@Override
+			public Object doInJpa(EntityManager manager)
+					throws PersistenceException {
+				FullTextEntityManager fullTextEntityManager = Search
+						.getFullTextEntityManager(manager);
+				try {
+					fullTextEntityManager.createIndexer(BaseBean.class)
+							.startAndWait();
+				} catch (InterruptedException e) {
+					logger.error(e.getMessage(), e);
+				}
+				return null;
+			}
+		});
+	}
+
 	UserService userService;
-	
+
 	@Override
 	public BaseBean getById(final long id) {
 		return getJpaTemplate().find(BaseBean.class, id);
@@ -26,7 +54,7 @@ public class JpaBaseService extends JpaDaoSupport implements BaseService {
 		final User currentUser = userService.getCurrentUser();
 		final BaseBean bean = getById(id);
 		final String openIdUrl = bean.getCreator().getOpenIdUrl();
-		if(openIdUrl != null && openIdUrl.equals(currentUser.getOpenIdUrl())) {
+		if (openIdUrl != null && openIdUrl.equals(currentUser.getOpenIdUrl())) {
 			getJpaTemplate().remove(bean);
 		}
 		return bean;
@@ -48,7 +76,12 @@ public class JpaBaseService extends JpaDaoSupport implements BaseService {
 			@Override
 			public List<BaseBean> doInJpa(final EntityManager entityManager)
 					throws PersistenceException {
-				return entityManager.createQuery("select object (b) from " + BaseBean.class.getName()+ " b where date_trunc('day',b.created) = :day").setParameter("day", day).getResultList();
+				return entityManager
+						.createQuery(
+								"select object (b) from "
+										+ BaseBean.class.getName()
+										+ " b where date_trunc('day',b.created) = :day")
+						.setParameter("day", day).getResultList();
 			}
 		});
 	}
@@ -61,8 +94,39 @@ public class JpaBaseService extends JpaDaoSupport implements BaseService {
 			@Override
 			public List<Date> doInJpa(final EntityManager entityManager)
 					throws PersistenceException {
-				//TODO: this runs on postgresql only
-				return entityManager.createNativeQuery("SELECT distinct date_trunc('day',created) from base").getResultList();
+				// TODO: this runs on postgresql only
+				return entityManager.createNativeQuery(
+						"SELECT distinct date_trunc('day',created) from base")
+						.getResultList();
+			}
+		});
+	}
+
+	@Override
+	public List<BaseBean> search(final String freeText) {
+		return getJpaTemplate().execute(new JpaCallback<List<BaseBean>>() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public List<BaseBean> doInJpa(EntityManager em)
+					throws PersistenceException {
+				// TODO Auto-generated method stub
+				final FullTextEntityManager fullTextEntityManager = org.hibernate.search.jpa.Search
+						.getFullTextEntityManager(em);
+				final MultiFieldQueryParser parser = new MultiFieldQueryParser(
+						Version.LUCENE_29, new String[] {"text","description","shortDescr","displayName"},
+						new StandardAnalyzer(Version.LUCENE_29));
+				try {
+					final org.apache.lucene.search.Query query = parser
+							.parse(freeText);
+					javax.persistence.Query persistenceQuery = fullTextEntityManager
+							.createFullTextQuery(query, BaseBean.class);
+					return persistenceQuery.getResultList();
+				} catch (ParseException e) {
+					logger.warn(e.getMessage(), e);
+				}
+
+				return null;
 			}
 		});
 	}
