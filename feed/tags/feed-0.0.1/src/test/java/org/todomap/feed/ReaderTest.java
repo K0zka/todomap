220ab4cbeee673b26751445db50f2eb0faa34c51
laszlo.hornyak@gmail.com <laszlo.hornyak@gmail.com@e834a378-9303-11de-97c9-d5744c8abab9>
2012-11-20 23:16:37 +0000

    [maven-release-plugin]  copy for tag feed-0.0.1

---
+++ feed/tags/feed-0.0.1/src/test/java/org/todomap/feed/ReaderTest.java
@@ -0,0 +1,62 @@
+package org.todomap.feed;
+
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.todomap.feed.beans.Channel;
+import org.todomap.feed.beans.NewsFeed;
+import org.todomap.feed.beans.NewsItem;
+import org.todomap.feed.beans.Rss;
+
+public class ReaderTest {
+
+	@Test
+	public void testAtom() throws IOException {
+		check("http://iwillworkforfood.blogspot.com/feeds/posts/default");
+		check("http://iwillworkforfood.blogspot.com/feeds/posts/default?alt=atom");
+	}
+
+	@Test(expected=IOException.class)
+	public void testFail() throws IOException {
+		check("http://intenitonallynotexistingdomain.dictat.org");
+	}
+
+	@Test
+	public void testRSS() throws IOException {
+		check("http://planet.postgresql.org/rss20.xml");
+		check("http://pulispace.com/en/media/blog?format=feed&type=rss");
+//		check("https://twitter.com/statuses/user_timeline/kozka.rss");
+		check("http://iwillworkforfood.blogspot.com/feeds/posts/default?alt=rss");
+		check("http://tifyty.wordpress.com/feed/");
+	}
+
+	private void check(String url) throws IOException {
+		NewsFeed feed = Reader.read(url);
+		Assert.assertNotNull(feed);
+		for(NewsItem item : feed.getNewsItems()) {
+			Assert.assertNotNull(item);
+			Assert.assertNotNull(item.getTitle());
+		}
+	}
+
+	@Test
+	public void serialize() throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(Rss.class.getPackage()
+				.getName());
+		Marshaller marshaller = context.createMarshaller();
+		Rss rss = new Rss();
+		Channel channel = new Channel();
+		channel.setDescription("just a test");
+		channel.setCategory("test");
+		channel.setTitle("rss serialization test");
+		marshaller.marshal(rss, System.out);
+
+	}
+
+}
